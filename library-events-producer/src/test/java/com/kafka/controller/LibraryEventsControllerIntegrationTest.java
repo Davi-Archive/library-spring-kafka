83665807382304c71d8152e2f5f8a2cb9c237603
d1av <davi4alves@gmail.com>
2023-02-16 13:07:40 -0300

    test: :test_tube: add message checker on consumer

--- library-events-producer/src/test/java/com/kafka/controller/LibraryEventsControllerIntegrationTest.java
+++ library-events-producer/src/test/java/com/kafka/controller/LibraryEventsControllerIntegrationTest.java
@@ -2,6 +2,16 @@ package com.kafka.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,15 +22,45 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.context.TestPropertySource;
 
 import com.kafka.domain.Book;
 import com.kafka.domain.LibraryEvent;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EmbeddedKafka(topics = { "library-events" }, partitions = 3)
+@TestPropertySource(properties = {
+	"spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
+	"spring.kafka.admin.bootstrap.servers=${spring.embedded.kafka.brokers}" })
 class LibraryEventsControllerIntegrationTest {
 
     @Autowired
+    EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @Autowired
     TestRestTemplate restTemplate;
+
+    private Consumer<Integer, String> consumer;
+
+    @BeforeEach
+    void setUp() {
+	Map<String, Object> configs = new HashMap<>(
+		KafkaTestUtils.consumerProps("group1", "true",
+			embeddedKafkaBroker));
+	consumer = new DefaultKafkaConsumerFactory(configs,
+		new IntegerDeserializer(), new StringDeserializer())
+		.createConsumer();
+	embeddedKafkaBroker.consumeFromAllEmbeddedTopics(consumer);
+    }
+
+    @AfterEach
+    void tearDown() {
+	consumer.close();
+    }
 
     @Test
     void postLibraryEvent() {
@@ -42,9 +82,16 @@ class LibraryEventsControllerIntegrationTest {
 		.exchange("/v1/libraryevent", HttpMethod.POST,
 			request, LibraryEvent.class);
 	// then
-	
-	assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
-	
+
+	assertEquals(HttpStatus.CREATED,
+		responseEntity.getStatusCode());
+
+	ConsumerRecord<Integer, String> consumerRecord = KafkaTestUtils
+		.getSingleRecord(consumer, "library-events");
+
+	String expectedRecord = "{\"libraryEventId\":null,\"libraryEventType\":\"NEW\",\"book\":{\"bookId\":123,\"bookName\":\"Kafka using Spring Boot\",\"bookAuthor\":\"OkayChamp\"}}";
+	String value = consumerRecord.value();
+	Assertions.assertEquals(expectedRecord, value);
     }
 
 }
