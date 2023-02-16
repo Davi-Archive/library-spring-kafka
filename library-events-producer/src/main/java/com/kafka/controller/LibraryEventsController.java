71cf672ae4be3d245b28407f71a61158b9180031
d1av <davi4alves@gmail.com>
2023-02-16 02:20:09 -0300

    feat: :sparkles: add library controller

---
+++ library-events-producer/src/main/java/com/kafka/controller/LibraryEventsController.java
@@ -0,0 +1,20 @@
+package com.kafka.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kafka.domain.LibraryEvent;
+
+@RestController
+public class LibraryEventsController {
+
+    @PostMapping("/v1/libraryevent")
+    public ResponseEntity<LibraryEvent> postLibraryEvent(
+	    @RequestBody LibraryEvent libraryEvent) {
+	return ResponseEntity.status(HttpStatus.CREATED)
+		.body(libraryEvent);
+    }
+}
