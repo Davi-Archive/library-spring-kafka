a0f8bae5726b47dc30909a72bf099ff27687d76f
d1av <davi4alves@gmail.com>
2023-02-16 16:49:54 -0300

    feat: :sparkles: Consumer implemented

---
+++ library-events-consumer/src/main/java/com/kafka/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,10 @@
+package com.kafka.config;
+
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableKafka
+public class LibraryEventsConsumerConfig {
+
+}
